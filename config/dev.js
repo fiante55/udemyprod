bd6a14d0e717dc358f837076adb3d6a27d34edfa
Simon <lebouthilliers@gmail.com>
2018-07-08 23:49:16 -0400

    dad

--- config/dev.js
+++ config/dev.js
@@ -4,7 +4,7 @@
 
 // We use this file to store the secret keys and make sure it will not be
 // shared on git via .gitignore
-
+//
 module.exports = {
   googleClientID: '779149476955-hj5p492b79m2c09dmds59doqvfdvnmtc.apps.googleusercontent.com',
   googleClientSecret: 'JycR9fooHOsgY3ggxSx1OSOi',
